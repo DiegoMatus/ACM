c739c27fd41dde0d25d12f2916fe5ff4cbd30ea5
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-09-26 22:15:41 -0500

    adjusted tabs and spaces and style

--- 2015-09-26/F/main.cpp
+++ 2015-09-26/F/main.cpp
@@ -3,53 +3,50 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main() {
 	long casos, J, C, T, i, ganadores, jugada, p;
 	cin >> casos;
-	for(int i = 0; i<casos; i++){
-        cin >> J >> C >> T;
-        ganadores = 0;
-        long players[J+1];
-        long casillas[C+1];
+	for (int i = 0; i<casos; i++) {
+		cin >> J >> C >> T;
+		ganadores = 0;
+		long players[J+1];
+		long casillas[C+1];
 
-        for (i=1; i<=J; i++) {
-            players[i] = 1;
-        }
-        
-        for (i=1; i<=C; i++) {
-            cin >> casillas[i];
-        }
+		for (i=1; i<=J; i++) {
+			players[i] = 1;
+		}
 
-        for (i = 1; i <= T; i++) {
-            cin >> jugada;
- 
-            p = (i-1)%J + 1;
-            
-            while (players[p] == C) {
-                p = p%J + 1;
-            }
-            
-            players[p] += jugada;
-            players[p] += casillas[players[p]];
-            
-            if (players[p] == C) {
-                if (ganadores>0) {
-                    cout << " ";
-                }
-                cout << p;
-                ganadores++;
-            }
-        }
-        
-        if (ganadores==0) {
-            cout << -1 << endl;
-        } else {
-            cout << endl;
-        }
-    }
-    
-    while(true);
+		for (i=1; i<=C; i++) {
+			cin >> casillas[i];
+		}
 
-	return 0;     
+		for (i = 1; i <= T; i++) {
+			cin >> jugada;
+
+			p = (i-1)%J + 1;
+
+			while (players[p] == C) {
+				p = p%J + 1;
+			}
+
+			players[p] += jugada;
+			players[p] += casillas[players[p]];
+
+			if (players[p] == C) {
+				if (ganadores>0) {
+					cout << " ";
+				}
+				cout << p;
+				ganadores++;
+			}
+		}
+
+		if (ganadores==0) {
+			cout << -1 << endl;
+		} else {
+			cout << endl;
+		}
+	}
+
+	return 0;
 }
--- 2015-09-26/I/main.cpp
+++ 2015-09-26/I/main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 int main() {
 	int n, len, coincide;
-    char cadena1[100];
-    char cadena2[100];
-	
+	char cadena1[100];
+	char cadena2[100];
+
 	cin >> n;
-	
-    for(int i=0; i<n; i++) {
-        scanf("%s" , cadena1);
-        scanf("%s" , cadena2);
-        coincide = 0;
 
-        len = strlen(cadena1);
+	for (int i=0; i<n; i++) {
+		scanf("%s" , cadena1);
+		scanf("%s" , cadena2);
+		coincide = 0;
 
-        for (int j=0; j<len; j++) {
-            if (cadena1[j] != cadena2[j]) {
-                coincide++;
-            }
-        }
-        cout << coincide << endl;
-    }
+		len = strlen(cadena1);
 
-	return 0;     
+		for (int j=0; j<len; j++) {
+			if (cadena1[j] != cadena2[j]) {
+				coincide++;
+			}
+		}
+		cout << coincide << endl;
+	}
+
+	return 0;
 }
